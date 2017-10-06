f89e3ecb629f827e56186b8d5b8ebcfb33400596
Mar Koss <kossmar2@gmail.com>
2017-10-06 13:55:11 -0700

    started project and generated random value for price

---
+++ W1D5-Payments/main.m
@@ -0,0 +1,20 @@
+//
+//  main.m
+//  W1D5-Payments
+//
+//  Created by Mar Koss on 2017-10-06.
+//  Copyright © 2017 Mar Koss. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+       
+        int price = arc4random_uniform(1000) + 100;
+        
+        
+        
+    }
+    return 0;
+}
